c2537c5fa107e6c07dfae784b4029c128d8dcd62
Ankit Mod <ankit300mod@gmail.com>
2024-07-31 12:32:48 +0530

    d

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,11 +3,13 @@ import './Footer.css';
 
 const Footer = () => {
   return ( 
-    <footer className="footer">
+    <footer className="footer d-flex justify-content-center Homor">
+      <div>
       <p>© 2024 Universal Translator</p>
       <div className="footer-links">
         <a href="#">Privacy Policy</a> 
         <a href="#">Contact Us</a>
+        </div>
       </div>
     </footer>
   );
